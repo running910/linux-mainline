0be6db6c16e134b75846ac4a34185b33779ecc9d
Zhixing Chen <running910@gmail.com>
2025-08-13 18:37:16 +0800

    garble: optimize code

--- include/net/netfilter/nf_garble.h
+++ include/net/netfilter/nf_garble.h
@@ -1,11 +1,11 @@
 #ifndef _NF_GARBLE_H
 #define _NF_GARBLE_H
 
-void garble_insert_tcp_packet(__be32 saddr, __be32 daddr, __be16 sport, __be16 dport, const struct sock *sk);
+extern void garble_insert_tcp_packet(__be32 saddr, __be32 daddr, __be16 sport, __be16 dport, const struct sock *sk);
 
-void garble_insert_tcp_packet_v6(const struct in6_addr *saddr, const struct in6_addr *daddr, __be16 sport, __be16 dport, const struct sock *sk);
+extern void garble_insert_tcp_packet_v6(const struct in6_addr *saddr, const struct in6_addr *daddr, __be16 sport, __be16 dport, const struct sock *sk);
 
-void garble_insert_udp_packet(struct sk_buff *skb);
+extern void garble_insert_udp_packet(struct sk_buff *skb);
 
 static inline bool check_if_bypass_conntrack(struct sk_buff *skb)
 {
