77c16d4b4046af50ef626fef4a02083431e01a3a
Zhixing Chen <running910@gmail.com>
2025-12-03 10:20:40 +0800

    garble: remove unnecessary http header

--- net/netfilter/garble/http.c
+++ net/netfilter/garble/http.c
@@ -1,12 +1,19 @@
 #include <linux/string.h>   // for memset / memcpy
 #include <linux/random.h>
 
+#if 0
 static const char *http_fmt =
 	"GET / HTTP/1.1\r\n"
 	"Host: %s\r\n"
 	"Accept: */*\r\n"
 	"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
 	"(KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36\r\n"
+	"\r\n";
+#endif
+
+static const char *http_fmt =
+	"GET / HTTP/1.1\r\n"
+	"Host: %s\r\n"
 	"\r\n";
 
 inline unsigned char *build_http_request(unsigned char *buf, int *len, const char *host)
